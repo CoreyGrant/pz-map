2b94d7fcf8f3e5b6908eb795c345c3a5f09a477e
corey <corey.grant.durham@hotmail.co.uk>
2025-08-12 01:44:17 +0100

    Updated reset all data to work on desktop

--- PzMap/wwwroot/index.js
+++ PzMap/wwwroot/index.js
@@ -159,7 +159,7 @@
     }
 
     // hacky reset behaviour
-    const saveManagerNewLabel = document.querySelector("#save-manager-new label")
+    const saveManagerNewLabel = document.querySelector("#save-manager legend")
     let tapCounter = 0;
     let timeout;
     saveManagerNewLabel.addEventListener('touchstart', function () {
@@ -168,6 +168,19 @@
         timeout = setTimeout(() => tapCounter = 0, 1000);
     });
     saveManagerNewLabel.addEventListener('touchend', function () {
+        if (tapCounter > 8) {
+            if (confirm("Delete all saved data?")) {
+                localStorage.clear();
+                window.location.reload();
+            }
+        }
+    });
+    saveManagerNewLabel.addEventListener('mousedown', function () {
+        tapCounter++;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => tapCounter = 0, 1000);
+    });
+    saveManagerNewLabel.addEventListener('mouseup', function () {
         if (tapCounter > 8) {
             if (confirm("Delete all saved data?")) {
                 localStorage.clear();
